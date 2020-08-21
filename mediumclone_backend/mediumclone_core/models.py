c2acb611a4b77fe0fb08d78406753e5a0cfcd568
Bihan Viranga <bihanviranga@gmail.com>
2020-08-21 21:16:55 +0530

    Added basic models

--- mediumclone_backend/mediumclone_core/models.py
+++ mediumclone_backend/mediumclone_core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    content = models.TextField()
+    featured_image = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
